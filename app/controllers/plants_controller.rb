f3972b068782eaac589302a59ad754d7d852a50b
fartun <fartunwario34@gmail.com>
2023-06-23 23:56:51 +0300

    completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,4 @@
 class PlantsController < ApplicationController
-
   # GET /plants
   def index
     plants = Plant.all
@@ -18,9 +17,29 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find(params[:id])
+    if plant.update(plant_params)
+      render json: plant, status: :ok
+    else
+      render json: { error: "Failed to update plant" }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find(params[:id])
+    if plant.destroy
+      head :no_content
+    else
+      render json: { error: "Failed to delete plant" }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def plant_params
-    params.permit(:name, :image, :price, :is_in_stock)
+    params.require(:plant).permit(:is_in_stock)
   end
 end
